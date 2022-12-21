581aa27c7381cb4193434628ec12f0eedf022385
Teena Thakur <teenathakur4050@gmail.com>
2022-12-21 19:18:48 +0530

    Add files via upload

---
+++ Patterns/Hollow_Rhombus.java
@@ -0,0 +1,32 @@
+package lectureFunction;
+
+public class Hollow_Rhombus {                           
+    public static void Rhymbous(int n){       
+        for(int i=1;i<=n;i++){               
+                                             
+            for(int j=n;j>i;j--){
+                    System.out.print(" ");
+            }
+    
+                for(int j=1;j<=n;j++){
+                    if(i==1||i==n||j==1||j==n){
+                        System.out.print(("*"));
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                
+            System.out.println();
+        }
+            }
+            public static void main(String[] args){
+                Rhymbous(5);
+
+
+
+        }
+
+    }
+    
+
