538cb147c3dd55ab1d9080569eeeb355c0b2d191
Teena Thakur <teenathakur4050@gmail.com>
2022-12-21 18:58:36 +0530

    Add files via upload

---
+++ Patterns/Alphabet.java
@@ -0,0 +1,15 @@
+public class characterloop {
+    public static void main(String[] args){
+        char n=65;
+
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print (n);
+                n+=1;
+            }System.out.println();
+             
+        }
+
+    }
+    
+}
