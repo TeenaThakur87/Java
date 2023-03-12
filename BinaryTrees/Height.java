3dae4aa3b5426ddb7f7059cc158795a23ec5ac77
Teena Thakur <teenathakur4050@gmail.com>
2023-03-12 11:29:22 +0530

    calculate height of tree

--- BinaryTrees/Height.java
+++ BinaryTrees/Height.java
@@ -38,7 +38,7 @@ public class Height {
             root.left.left=new Node(4);
             root.left.right=new Node(5);
             root.right.left=new Node(6);
-            root.right.left=new Node(7);
+            root.right.right=new Node(7);
 
             System.out.println(heigth(root));
         }
